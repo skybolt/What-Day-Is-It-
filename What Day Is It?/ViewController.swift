52b33a5b8f4e9291b763048d3ea6428f63a7807f
rob cooper <skybolt@gmail.com>
2020-05-25 17:41:16 -0700

    Initial Commit

---
+++ What Day Is It?/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  What Day Is It?
+//
+//  Created by rob on 5/25/20.
+//  Copyright © 2020 rob. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
